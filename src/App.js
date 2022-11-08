4de1aa01ea9b7ae25478bbb67f43245bb5eee5a7
dhani <dhanyanarayan28@gmail.com>
2022-11-07 19:18:53 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import React from 'react';
+import background from '../src/backgroundimg.jpeg';
 
 function App() {
   return (
-    <div>
-      
+    <div style={{
+      backgroundImage: `url(${background})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      height:'100vh',
+      width:'100vw',
+      margin: '0px',
+    }}>
+      Hello World
     </div>
   );
 }
