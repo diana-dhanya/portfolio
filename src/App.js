4a63e5223d3b4017d7140c05a4700d0f1a0c7f75
dhani <dhanyanarayan28@gmail.com>
2022-11-12 17:39:00 -0600

    init icons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import background from '../src/backgroundimg.jpeg';
 import * as React from 'react';
 import { Button } from '@mui/material';
-//import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
 
 function App() {
   return (
@@ -17,8 +17,14 @@ function App() {
       margin: '0px',
     }}>
       <div style={{fontSize: '50px', flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
-        <Button variant="contained" onClick={()=>window.open('https://reactjs.org', '_blank')}>
-          Click Me
+        <Button variant="contained" onClick={()=>window.open('https://reactjs.org', '_blank')} 
+        style={{color:'black', backgroundColor:'#bdcfe1', borderRadius:50, fontFamily:'Futura'}}>
+           
+        </Button>
+        &emsp;
+        <Button variant="contained" onClick={()=>window.open('https://reactjs.org', '_blank')} 
+        style={{color:'black', backgroundColor:'#bdcfe1', borderRadius:50, fontFamily:'Futura'}}>
+          main 
         </Button>
       </div>
     </div>
